6daf64f00065a1ac4bc2973a71a89680388b83f9
Yamuna PG <150881590+yamuna-FSD-Developer@users.noreply.github.com>
2024-03-03 20:40:05 +0530

    Update Head.jsx

--- src/Task/Head.jsx
+++ src/Task/Head.jsx
@@ -5,7 +5,7 @@ function Head() {
     <div className="container text-center" >
       <div className="row">
         <div className="col-lg-12">
-          <h1 className="pt-2 pb-2" style={{color:"#000066"}}>React Task Status</h1>
+          <h1 className="pt-2 pb-2" style={{color:"#000066"}}>MY ToDo</h1>
         </div>
       </div>
     </div>
